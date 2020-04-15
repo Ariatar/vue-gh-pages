d7d606cd984a5e2ee7968929903d7fa350b6e24b
Ariatar <hellgate9292@gmail.com>
2020-04-15 20:16:39 +0300

    first commit

---
+++ precache-manifest.9503022985cce48408d69be178e70e1e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7755dc58f78e91020ba4",
+    "url": "/vue-gh-pages/css/app.14adbbe3.css"
+  },
+  {
+    "revision": "7a2cf9c91b18cbdfeaff",
+    "url": "/vue-gh-pages/css/chunk-vendors.07ea5ded.css"
+  },
+  {
+    "revision": "ecc54158f866a1415d8a84c78de66b76",
+    "url": "/vue-gh-pages/images/mirana-avatar-big.png"
+  },
+  {
+    "revision": "70229d6bcaae131ec5c07811cbad1fbf",
+    "url": "/vue-gh-pages/images/mirana-avatar.png"
+  },
+  {
+    "revision": "85da3304b1afc603aa143a6db9005c20",
+    "url": "/vue-gh-pages/images/traxex-avatar-big.png"
+  },
+  {
+    "revision": "fcc0fb8b096122c487d56e4845d54729",
+    "url": "/vue-gh-pages/images/traxex-avatar.png"
+  },
+  {
+    "revision": "52379e53f3e2caf37c9542c2bfc804ca",
+    "url": "/vue-gh-pages/images/wind-avatar-big.png"
+  },
+  {
+    "revision": "0f0e309d2ea1fd230cd121402b02f4fd",
+    "url": "/vue-gh-pages/images/wind-avatar.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-gh-pages/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "be59b0d7a1af553f5dc08f61c3a915d9",
+    "url": "/vue-gh-pages/index.html"
+  },
+  {
+    "revision": "cad38d8ea4f2294eaa96",
+    "url": "/vue-gh-pages/js/about.5b81798a.js"
+  },
+  {
+    "revision": "7755dc58f78e91020ba4",
+    "url": "/vue-gh-pages/js/app.0bc0252b.js"
+  },
+  {
+    "revision": "7a2cf9c91b18cbdfeaff",
+    "url": "/vue-gh-pages/js/chunk-vendors.5abfecbb.js"
+  },
+  {
+    "revision": "f5a04f0c311c54c6fa20558a5d928f37",
+    "url": "/vue-gh-pages/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-gh-pages/robots.txt"
+  },
+  {
+    "revision": "caae00db37660820b4449052b5dd0540",
+    "url": "/vue-gh-pages/scss/_variables.scss"
+  },
+  {
+    "revision": "905978c3c7ce1902d23ff4b82bd40ea1",
+    "url": "/vue-gh-pages/scss/btn.scss"
+  },
+  {
+    "revision": "2d5a0ff55dae374ada3e933472944fd0",
+    "url": "/vue-gh-pages/scss/icons.scss"
+  },
+  {
+    "revision": "90ff0c3fb04ded64a500e9b88205dd58",
+    "url": "/vue-gh-pages/scss/main.scss"
+  },
+  {
+    "revision": "1595e02fb1d8e39d65f6526cfda12661",
+    "url": "/vue-gh-pages/scss/navbar.scss"
+  },
+  {
+    "revision": "d71241501bc6c7db11d558ad53a673f9",
+    "url": "/vue-gh-pages/scss/other.scss"
+  },
+  {
+    "revision": "fd34d9704629cf2013641f8372fb38d7",
+    "url": "/vue-gh-pages/scss/test.scss"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/vue-gh-pages/scss/typography.scss"
+  }
+]);
